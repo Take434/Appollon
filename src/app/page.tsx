f51fb1f019e379a0024a2056060b09ce5cf8f338
Tim Herr <timherr7@web.de>
2023-05-16 08:28:44 +0200

    tested prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { validatedTestingAction } from '@/server-actions/test.action';
-import Image from 'next/image'
 import { useState } from 'react';
 
 export default function Home() {
   const [msg, setMsg] = useState<string>("nothing here yet");
 
   return (
-      <div>
+      <div className="">
         <button onClick={() => {
           validatedTestingAction({ stuff: "test" }).then((response) => setMsg(response.message));
         }}>
-        Run server action
-      </button>
-      
-      <h1>{msg}</h1>
-    </div>
+          Run server action
+        </button>
+        
+        <h1>{msg}</h1>
+      </div>
   )
 }
