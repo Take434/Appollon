5b062169acbf83d78b1b2945bd5c67a50b082d6e
Giesen, Jonas <jonas.giesen@hsrw.org>
2023-05-26 17:02:43 +0200

    created first apiModels and api parsing functions

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+/**
+ * The home page.
+ * @returns The home page.
+ */
+export default function Home2() {
+  return <div></div>;
+}
---
+++ src/models/apiModels/apiModels.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const apiUser = z.object({
+  country: z.string().length(2),
+  displayName: z.string(),
+  email: z.string(),
+  id: z.string(),
+  images: z.object({
+    url: z.string(),
+  }),
+});
+
+export const apiArtist = z.object({
+  followers: z.object({
+    total: z.number(),
+  }),
+  genres: z.string().array().nullable(),
+  id: z.string(),
+  name: z.string(),
+});
