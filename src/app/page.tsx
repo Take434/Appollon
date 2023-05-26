38393436da6de0039bae0723e09b0cb828e19100
Tim Herr <t.herr@milestone-consult.de>
2023-05-26 10:10:18 +0200

    configured linter and node version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,29 @@
 "use client";
 
-import { validatedTestingAction } from '@/server-actions/test.action';
-import { useState } from 'react';
+import { validatedTestingAction } from "@/server-actions/test.action";
+import { useState } from "react";
+import React from "react";
 
+/**
+ * The home page.
+ * @returns The home page.
+ */
 export default function Home() {
   const [msg, setMsg] = useState<string>("nothing here yet");
 
   return (
-      <div>
-        <button onClick={() => {
-          validatedTestingAction({ stuff: "hallihallo" }).then((response) => setMsg(response.message));
-        }}>
-          Run server action
-        </button>
-        
-        <h1>{msg}</h1>
-      </div>
-  )
+    <div>
+      <button
+        onClick={() => {
+          validatedTestingAction({ stuff: "hallihallo" }).then((response) =>
+            setMsg(response.message)
+          );
+        }}
+      >
+        Run server action
+      </button>
+
+      <h1>{msg}</h1>
+    </div>
+  );
 }
--- src/server-actions/test.action.ts
+++ src/server-actions/test.action.ts
@@ -1,19 +1,23 @@
 "use server";
 
-import { z } from 'zod';
-import { zact } from 'zact/server';
-import { PrismaClient } from '@prisma/client';
+import { z } from "zod";
+import { zact } from "zact/server";
+import { PrismaClient } from "@prisma/client";
 
-export const validatedTestingAction = zact(z.object({ stuff: z.string().min(3) }))(
-  async (input) => {
+/**
+ * This is a test action that is used to test the server side actions.
+ * @param input - The input to the action.
+ * @returns The output of the action.
+ */
+export const validatedTestingAction = zact(
+  z.object({ stuff: z.string().min(3) })
+)(async (input) => {
+  const prisma = new PrismaClient();
+  const jonas = await prisma.user.findFirst({
+    where: {
+      name: "JONAS",
+    },
+  });
 
-    const prisma = new PrismaClient();
-    const jonas = await prisma.user.findFirst({
-      where: {
-        name: "JONAS"
-      }
-    });
-
-    return { message: `hello ${jonas?.email}  ${input.stuff}` };
-  }
-);
+  return { message: `hello ${jonas?.email}  ${input.stuff}` };
+});
