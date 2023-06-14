3b78d2caf5a5c3d3cceba1c650d0cc86a379f1d6
Tim Herr <t.herr@milestone-consult.de>
2023-06-14 14:22:41 +0200

    added temporary fix for login bug in dev

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,16 @@
 
 import { RedirectSpotifyAction } from "@/server-actions/spotifyHandling.action";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Login() {
   const router = useRouter();
+
+  //THIS IS A TEMPORARY FIX FOR A BUG ONLY HAPPENING IN DEV MODE
+  //THIS HAS TO BE REMOVED ONCE THE APP IS DEPLOYED
+  useEffect(() => {
+    fetch("/welcome");
+  });
 
   return (
     <>
