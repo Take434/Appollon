873c7da8393c9785b1a8bab03d0ed60108a55d0b
Take434 <timh4376@gmail.com>
2023-07-29 13:44:06 +0200

    working on convert wiki

--- .github/concat-wiki.sh
+++ .github/concat-wiki.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-touch ../merged.md
-touch ../toc.md
+touch output/merged.md
+touch output/toc.md
 
 echo "# Appollon
 ![](images/AppollonLogo.svg)  
 _Appollon Logo, created by Tim_
-## Table of Contents" >> ../toc.md
+## Table of Contents" >> output/toc.md
 
 cat _Sidebar.md | while read l; do
 	if [[ ${l:0:3} != "###" ]]; then
@@ -21,24 +21,24 @@ cat _Sidebar.md | while read l; do
 	p="./$l.md"
 
 	if test -f $p; then
-		echo "- [$l](#$l)" >> ../toc.md
+		echo "- [$l](#$l)" >> output/toc.md
 
 		if [[ $(head $p -n1) != "# $l" ]]; then
 			cat <(echo "# $l") $p > new_$l.md
 			mv new_$l.md $p
 		fi
 
-		echo >> ../merged.md
+		echo >> output/merged.md
 
 		if [[ $l == "home" ]]; then
-			echo "# home" >> ../merged.md
-			tail -n +6 $p >> ../merged.md
+			echo "# home" >> output/merged.md
+			tail -n +6 $p >> output/merged.md
 		else
-			cat $p >> ../merged.md
+			cat $p >> output/merged.md
 		fi
 	fi
 done
 
-cat <(cat ../toc.md) ../merged.md > ../new_merged.md
-mv ../new_merged.md ../merged.md
-rm ../toc.md
+cat <(cat output/toc.md) output/merged.md > output/new_merged.md
+mv output/new_merged.md output/merged.md
+rm output/toc.md
